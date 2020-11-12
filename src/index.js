f9129e7eaede8cb213ac9a2c1c2f83b572a20380
Yoonwhan Ahn <yahn@Yoonwhans-MacBook-Pro.local>
2020-11-13 00:44:41 +0900

    End of section 3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+import React from 'react'
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css';
 import App from './app/layout/App';
-import reportWebVitals from './reportWebVitals';
 
 const rootEl = document.getElementById('root')
 
@@ -17,7 +18,4 @@ if (module.hot) {
 
 render();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
